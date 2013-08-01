ff5816a539d56532f3461cc5931e1d0596901367
kharhys <kelgitonga@gmail.com>
2013-08-01 23:00:12 +0300

    Initial Commit

---
+++ src/Kharhys/UserBundle/Entity/User.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Kharhys\UserBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use FOS\UserBundle\Model\User as BaseUser;
+
+/**
+ * User
+ *
+ * @ORM\Table()
+ * @ORM\Entity(repositoryClass="Kharhys\UserBundle\Entity\UserRepository")
+ */
+class User extends BaseUser
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+
+    /**
+     * Constructor
+     */
+    public function __construct()
+    {
+        parent::__construct();
+        // your own logic
+    }
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+}
---
+++ src/Kharhys/UserBundle/KharhysUserBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Kharhys\UserBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class KharhysUserBundle extends Bundle
+{
+}
